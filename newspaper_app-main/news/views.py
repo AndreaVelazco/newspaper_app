a97d1bd01ad9c1aaed44ab6404fe3094da1da934
Piero Omar De La Cruz Mancilla <pdelacruzm@ulasalle.edu.pe>
2025-06-13 17:36:52 -0500

    actualizando

--- newspaper_app-main/news/views.py
+++ newspaper_app-main/news/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def newsView(request):
-    return HttpResponse('Hello Web from view function. Ate, Maria Fernanda Pinazo 2025/06/13')
+    return HttpResponse('Hello Web Andrea Velazco')
